17a9d59f767658ccd7b4211502dd4c634e8c35b2
Melvin <melvinmps11301@gmail.com>
2025-03-06 17:30:26 +0300

    banner updated

--- src/components/homePage/BannerImageCarousel.js
+++ src/components/homePage/BannerImageCarousel.js
@@ -1,8 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
-// import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const images = [
   "/images/banner-image-1.jpg",
@@ -10,17 +9,35 @@ const images = [
   "/images/banner-image-3.jpg",
 ];
 
-export default function BannerImageCarousel() {
-  const [imageNum, setImageNum] = useState(1);
+export default function MainCarousel() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div className="relative h-[85vh] w-[100vw]">
-      <Image
-        src="/images/banner-image-1.jpg"
-        fill
-        className="object-cover object-top"
-        alt="Banner Carousel Image"
-      />
+    <div className="relative w-full overflow-hidden h-[85vh]">
+      <div
+        className="flex h-full transition-transform duration-700 ease-in-out"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {images.map((image, index) => (
+          <div key={index} className="min-w-full h-full shrink-0 relative">
+            <Image
+              priority
+              fill
+              src={image}
+              alt={`Slide ${index + 1}`}
+              className="object-cover"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
