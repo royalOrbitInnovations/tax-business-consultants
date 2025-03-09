0012e442ea95f0807d82310d80a72b0cffeb0f01
Melvin <melvinmps11301@gmail.com>
2025-03-09 20:54:22 +0300

    why choose us done

--- src/components/aboutUs/WhyChooseUs.js
+++ src/components/aboutUs/WhyChooseUs.js
@@ -2,9 +2,9 @@ import TextWithBorder from "../TextWithBorder";
 
 export default function WhyChooseUs() {
   return (
-    <div>
+    <div className="px-[20rem] py-[10rem] flex flex-col gap-[5rem]">
       <TextWithBorder text="Why Chooe Us" />
-      <h3 className="text-[4rem]/20 w-[70%] max-14xl:w-[95%]">
+      <h3 className="text-[4rem]/20 w-[80%] max-14xl:w-[95%]">
         From feasibility study to business plan development, our expert team
         provides comprehensive company registration services in Qatar, ensuring
         a seamless launch for your entrepreneurial journey.
