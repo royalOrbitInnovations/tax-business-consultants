3d7d69452d194c656ed686c0cdeb88ff38813216
Melvin <melvinmps11301@gmail.com>
2025-03-13 12:30:59 +0300

    pushing live for review

--- src/components/homePage/BannerImageCarousel.js
+++ src/components/homePage/BannerImageCarousel.js
@@ -10,6 +10,25 @@ const images = [
   "/images/banner-image-3.webp",
 ];
 
+const textData = [
+  {
+    head1: "Tax Business consultant",
+    head2: "is a professional service provided by individuals or firms",
+    p: "Elevate Your Business to the next level",
+  },
+  {
+    head1: "Hold 100% company Ownership",
+    head2: "and get started with the registration in Qatar",
+    p: "You will always have the full control of your company",
+  },
+  {
+    head1: "Local Sponsorships",
+    head2:
+      "is to have a Qatari national as your sponsor to do business in the country",
+    p: "Make your business easier to run",
+  },
+];
+
 export default function MainCarousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -22,7 +41,7 @@ export default function MainCarousel() {
   }, []);
 
   return (
-    <div className="relative w-full overflow-hidden h-[85vh] max-6xl:h-[100vh] z-[-1]0">
+    <div className="relative w-full overflow-hidden h-[85vh] max-6xl:h-[100vh] z-[-1]">
       <div
         className="flex h-full transition-transform duration-700 ease-in-out"
         style={{ transform: `translateX(-${currentIndex * 100}%)` }}
@@ -40,7 +59,10 @@ export default function MainCarousel() {
           </div>
         ))}
       </div>
-      <CarouselOverlayText />
+      <CarouselOverlayText
+        textData={textData[currentIndex]}
+        currentIndex={currentIndex}
+      />
     </div>
   );
 }
--- src/components/homePage/CarouselOverlayText.js
+++ src/components/homePage/CarouselOverlayText.js
@@ -1,51 +1,109 @@
+// "use client";
+//
+// import ButtonUI from "../ButtonUI";
+// import { useEffect, useState } from "react";
+//
+// const data = [{}];
+//
+// export default function CarouselOverlayText({ textData }) {
+//   console.log(textData);
+//
+//   const [width, setWidth] = useState(0);
+//
+//   useEffect(() => {
+//     const screenWidth = () => {
+//       const width = window.innerWidth;
+//       setWidth(width);
+//     };
+//     screenWidth();
+//   }, []);
+//   return (
+//     <div className="absolute top-[55%] max-6xl:top-[50%] translate-y-[-50%] items-end justify-start text-left flex flex-col gap-[4rem] tracking-widest ">
+//       <h1 className="text-[7rem]/30 w-[50%] max-16xl:w-[70%] max-16xl:text-[6rem]/25 max-6xl:w-[80%] max-4xl:w-[100%]">
+//         <span className="text-(--ui-dark)">{textData.head1}</span>{" "}
+//         {textData.head2}
+//       </h1>
+//       <div className="flex gap-[1.5rem] items-center justify-start w-[50%] max-14xl:w-[60%] max-4xl:w-[80%] border-b border-gray-400 pb-[2rem]">
+//         <img
+//           src="/svg/arrorw-top-right-tax-business-consultants.svg"
+//           alt="Arrow SVG"
+//           className=" h-[4rem] w-[4rem]"
+//         />
+//         <p className="text-[2rem]/10 max-16xl:text-[1.7rem] max-6xl:text-[2rem] max-16xl:font-bold">
+//           {textData.p}
+//         </p>
+//       </div>
+//       {/* <hr className="bg-[--ui-dark]" /> */}
+//       <ButtonUI
+//         width={width <= 1600 ? "20rem" : "30rem"}
+//         size={width <= 1600 ? "1.8rem" : "2.2rem"}
+//         paddingY="1.8rem"
+//         to="/contact-us"
+//       >
+//         Explore More
+//         <img
+//           src="/svg/arrorw-top-right-tax-business-consultants.svg"
+//           alt="Arrow SVG"
+//           className="bg-white rounded-[50%] h-[2rem] w-[2rem]"
+//         />
+//       </ButtonUI>
+//     </div>
+//   );
+// }
+
 "use client";
 
-import ButtonUI from "../ButtonUI";
 import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import ButtonUI from "../ButtonUI";
 
-const data = [{}];
-
-export default function CarouselOverlayText() {
+export default function CarouselOverlayText({ textData, currentIndex }) {
   const [width, setWidth] = useState(0);
 
   useEffect(() => {
-    const screenWidth = () => {
-      const width = window.innerWidth;
-      setWidth(width);
-    };
-    screenWidth();
+    const updateWidth = () => setWidth(window.innerWidth);
+    updateWidth();
   }, []);
+
   return (
-    <div className="absolute top-[55%] max-6xl:top-[50%] translate-y-[-50%] left-[10%] flex flex-col gap-[4rem] tracking-widest ">
-      <h1 className="text-[7rem]/30 w-[50%] max-16xl:w-[70%] max-16xl:text-[6rem]/25 max-6xl:w-[80%] max-4xl:w-[100%]">
-        <span className="text-(--ui-dark)">Tax Business consultant</span> is a
-        professional service provided by individuals or firms
-      </h1>
-      <div className="flex gap-[1.5rem] w-[40%] max-14xl:w-[60%] max-4xl:w-[80%] border-b border-gray-400 pb-[2rem]">
-        <img
-          src="/svg/arrorw-top-right-tax-business-consultants.svg"
-          alt="Arrow SVG"
-          className=" h-[5rem] w-[5rem]"
-        />
-        <p className="text-[2rem]/10 max-16xl:text-[1.7rem] max-6xl:text-[2rem] max-16xl:font-bold">
-          Current business operations, identify areas of improvement, and
-          develop strategies amd solutions to addess.
-        </p>
-      </div>
-      {/* <hr className="bg-[--ui-dark]" /> */}
-      <ButtonUI
-        width={width <= 1600 ? "20rem" : "30rem"}
-        size={width <= 1600 ? "1.8rem" : "2.2rem"}
-        paddingY="1.8rem"
-        to="/contact-us"
-      >
-        Explore More
-        <img
-          src="/svg/arrorw-top-right-tax-business-consultants.svg"
-          alt="Arrow SVG"
-          className="bg-white rounded-[50%] h-[2rem] w-[2rem]"
-        />
-      </ButtonUI>
+    <div className="absolute top-[55%] max-6xl:top-[50%] translate-y-[-50%] right-[20rem] items-end justify-end text-left flex flex-col gap-[4rem] tracking-widest">
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={currentIndex}
+          initial={{ opacity: 0, x: width }}
+          animate={{ opacity: 1, x: 1000 }}
+          exit={{ opacity: 0, x: -500 }}
+          transition={{ duration: 0.7 }}
+        >
+          <h1 className="text-[7rem]/30 w-[50%] max-16xl:w-[70%] max-16xl:text-[6rem]/25 max-6xl:w-[80%] max-4xl:w-[100%]">
+            <span className="text-(--ui-dark)">{textData.head1}</span>
+            {textData.head2}
+          </h1>
+          <div className="flex gap-[1.5rem] items-center jutext-left w-[50%] max-14xl:w-[60%] max-4xl:w-[80%] border-b border-gray-400 pb-[2rem]">
+            <img
+              src="/svg/arrorw-top-right-tax-business-consultants.svg"
+              alt="Arrow SVG"
+              className="h-[4rem] w-[4rem]"
+            />
+            <p className="text-[2rem]/10 max-16xl:text-[1.7rem] max-6xl:text-[2rem] max-16xl:font-bold">
+              {textData.p}
+            </p>
+          </div>
+          <ButtonUI
+            width={width <= 1600 ? "20rem" : "30rem"}
+            size={width <= 1600 ? "1.8rem" : "2.2rem"}
+            paddingY="1.8rem"
+            to="/contact-us"
+          >
+            Explore More
+            <img
+              src="/svg/arrorw-top-right-tax-business-consultants.svg"
+              alt="Arrow SVG"
+              className="bg-white rounded-[50%] h-[2rem] w-[2rem]"
+            />
+          </ButtonUI>
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
