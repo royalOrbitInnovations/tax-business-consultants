a5a4592e9101cc83c9fd007f458049832b5d6bda
Melvin <melvinmps11301@gmail.com>
2025-03-14 22:17:56 +0300

    small improvements

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,18 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function logo({ type }) {
   return (
     <div className="relative w-[15rem] h-[6rem]">
-      <Image
-        fill
-        src={
-          type === "header"
-            ? "/images/logo-header-tax-business-consultants.webp"
-            : "/logo-footer-tax-business-consultants.webp"
-        }
-        alt="Brand Logo"
-        className="object-cover hover:scale-105 transition-all duration-300"
-      />
+      <Link href="/">
+        <Image
+          fill
+          src={
+            type === "header"
+              ? "/images/logo-header-tax-business-consultants.webp"
+              : "/logo-footer-tax-business-consultants.webp"
+          }
+          alt="Brand Logo"
+          className="object-cover hover:scale-105 transition-all duration-300"
+        />
+      </Link>
     </div>
   );
 }
