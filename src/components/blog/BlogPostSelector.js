80aa63723c38844da1db3e5f7cbf89426e2038f4
Melvin <melvinmps11301@gmail.com>
2025-03-16 13:08:45 +0300

    updated blogs page

--- src/components/blog/BlogPostSelector.js
+++ src/components/blog/BlogPostSelector.js
@@ -5,17 +5,17 @@ export default function BlogPostSelector({ blog }) {
   const { heading, content, image, id } = blog;
   return (
     <div className="flex flex-col gap-[2rem] w-[25vw] items-start  border border-(--ui-dark) rounded-[25px] px-[2rem] py-[2rem] mb-[3rem] hover:scale-102 hover:border-(--ui-dark) transition-all duration-300 shadow-xl max-15xl:w-[32vw] max-10xl:w-[38vw] max-6xl:w-[90vw] max-6xl:gap-[4rem]">
-      <div className="relative h-[28rem] w-[23vw] rounded-[25px] overflow-hidden shadow-xl max-15xl:w-[29vw] max-10xl:w-[35vw] max-6xl:w-[85vw]">
+      <div className="relative h-[30rem] w-[23vw] rounded-[25px] overflow-hidden shadow-xl max-15xl:w-[29vw] max-10xl:w-[35vw] max-6xl:w-[85vw]">
         <img
           src={image}
           alt={heading}
-          className="object-cover hover:scale-110 transition-all duration-800"
+          className="object-cover hover:scale-110 transition-all duration-800 h-[100%] w-[100%]"
         />
       </div>
       <Link href={`/blog/${id}`}>
         <h2 className="text-4xl font-bold">{heading}</h2>
       </Link>
-      <div className="h-[10rem] text-2xl overflow-hidden">
+      <div className="h-[10rem] text-[1.8rem] overflow-hidden">
         <ReactMarkdown>{`${content.substring(0, 200)}...`}</ReactMarkdown>
       </div>
       <Link href={`/blog/${id}`}>
