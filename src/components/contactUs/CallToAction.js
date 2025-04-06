545b7baf00dcb30b3fe76bb66f43b515f327ee03
Melvin <melvinmps11301@gmail.com>
2025-04-06 14:16:49 +0300

    spelling mistake updated

--- src/components/contactUs/CallToAction.js
+++ src/components/contactUs/CallToAction.js
@@ -4,7 +4,7 @@ export default function CallToAction() {
   return (
     <div className="bg-(--ui-dark) text-white px-[20rem] py-[5rem] flex justify-between items-center max-10xl:py-[10rem] max-6xl:px-[5rem]">
       <div>
-        <p className="text-3xl">Let&apos;s Build Togetehr</p>
+        <p className="text-3xl">Let&apos;s Build Together</p>
         <p className="text-7xl">Need to Rethink your Business Process?</p>
       </div>
       <button className="border px-[5rem] py-[2rem] text-3xl hover:bg-white hover:text-(--ui-dark) transition-all duration-300 cursor-pointer">
